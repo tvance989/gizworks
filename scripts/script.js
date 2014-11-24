4d5b328c4e4ba0952165e6a4210373a38ca40775
Taylor <tvance989@gmail.com>
2014-11-23 19:09:31 -0600

    Sassy responsive design

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,12 @@
 $(function(){
     var navOffset = $("nav").outerHeight();
+    if ($(window).width() <= 600) { // change as necessary (small breakpoint media query)
+        navOffset = 0;
+    }
 
     $("#home").css("margin-top", navOffset);
 
-    $("nav ul li").click(function(){;
+    $("nav ul li").click(function(){
         var top = $("#"+$(this).data("target")).offset().top - navOffset;
         $("html, body").animate({
             scrollTop: top
